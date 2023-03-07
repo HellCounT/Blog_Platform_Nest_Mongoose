d73fe343320939631c09a442f0a2e7a4a568b5cb
HellCounT <a_killaheadz@live.ru>
2023-03-07 16:11:09 +0300

    Test coding

---
+++ src/users/users.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  getUsers() {
+    return [{ id: 1 }, { id: 2 }];
+  }
+  @Post()
+  createUser(@Body() inputModel: CreateUserInputModelType) {
+    return {
+      id: 12,
+      name: inputModel.name,
+      childrenCount: inputModel.childrenCount,
+    };
+  }
+}
+
+type CreateUserInputModelType = {
+  name: string;
+  childrenCount: number;
+};
