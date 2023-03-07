004c31332bf7eef53fb87c3001b56eff4d9bd58d
HellCounT <a_killaheadz@live.ru>
2023-03-07 16:36:57 +0300

    Test coding

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,25 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(protected usersService: UsersService) {}
   @Get()
-  getUsers() {
-    return [{ id: 1 }, { id: 2 }];
+  getUsers(@Query() query: { term: string }) {
+    return this.usersService.findUsers(query.term);
+  }
+  @Get(':id')
+  getUser(@Param('id') userId: string) {
+    return [{ id: 1 }, { id: 2 }].find((u) => u.id === +userId);
   }
   @Post()
   createUser(@Body() inputModel: CreateUserInputModelType) {
@@ -12,6 +27,20 @@ export class UsersController {
       id: 12,
       name: inputModel.name,
       childrenCount: inputModel.childrenCount,
+    };
+  }
+  @Delete(':id')
+  deleteUser(@Param('id') userId: string) {
+    return;
+  }
+  @Put(':id')
+  updateUser(
+    @Param('id') userId: string,
+    @Body() model: CreateUserInputModelType,
+  ) {
+    return {
+      id: userId,
+      model: model,
     };
   }
 }
