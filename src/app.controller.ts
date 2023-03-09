f9c6d5b0e33ae7e2c395515db09aee365a4be7dc
HellCounT <a_killaheadz@live.ru>
2023-03-09 16:47:58 +0300

    h13: fixed vercel.json

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Blog, BlogDocument } from './blogs/blogs.schema';
+import { Model } from 'mongoose';
+import { Post, PostDocument } from './posts/posts.schema';
+import { User, UserDocument } from './users/users.schema';
+import { Comment, CommentDocument } from './comments/comments.schema';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @InjectModel(Blog.name) private blogModel: Model<BlogDocument>,
+    @InjectModel(Post.name) private postModel: Model<PostDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Delete('testing/all-data')
+  async deleteAllData() {
+    await this.blogModel.deleteMany({});
+    await this.postModel.deleteMany({});
+    await this.commentModel.deleteMany({});
+    await this.userModel.deleteMany({});
+    return;
   }
 }
